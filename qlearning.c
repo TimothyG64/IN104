0558cdd945138ad36d3936516326722d98aa442b
Zhou Yanyu（周妍妤） <earthazyy@hotmail.com>
2022-05-03 16:39:59 +0200

    Update qlearning.c

--- qlearning.c
+++ qlearning.c
@@ -1,12 +1,24 @@
 /*
  * @Author: your name
  * @Date: 2022-04-05 16:58:37
- * @LastEditTime: 2022-04-12 16:21:10
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-05-03 16:37:03
+ * @LastEditors: Zhou Yanyu（周妍妤） earthazyy@hotmail.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \CMaze-main\qlearning.c
  */
+
+#include <stdio.h>
 #include "mazeEnv.h"
+#include "mazeEnv.c"
+#include "functions.c"
+#include "functions.h"
+// Exploration factor
+#define double alpha = 0.1; // learning rate
+#define double epsilon = 0.1 ; 
+#define double gamma = 0.9 ; // discount factor
+#define double visited_mark = 0.8; // Cells visited by the rat will be painted by gray 0.8
+#define double rat_mark = 0.5; //# The current rat cell will be painteg by gray 0.5
+
 /* maze is a 2d array of character, there are 5 types of cells:
 unknown, wall, known, goal, crumb
 We have 4 action:
@@ -25,29 +37,33 @@ An attempt to crumb cells costs -0.25 and will not be executed
 To avoid infinite loops, the game is ended once the total reward is below the threshold -0.5*mazesize
 
 */
-//For each point on the table, return the reward
-double reward(int state){
-    double points = -0.04; 
-    double visited_mark = 0.8; // Cells visited by the rat will be painted by gray 0.8
-    double rat_mark = 0.5; //# The current rat cell will be painteg by gray 0.5
-    switch (expression)
-    {
-    case /* constant-expression */:
-        /* code */
+//For each point on the map, return the reward
+int RNG (int largestValue){
+    int k = rand() % (largestValue+1);
+    return k;
+}
+
+double reward(int row, int col){
+    double points;
+    switch (maze[row][col]){
+    case '+':
+        points = -1.5;
+        break;
+
+    case '.':
+        points = -0.25;
+        break;
+
+    case 'g':
+        points = 1000;
         break;
     
     default:
+        points = -0.04;
         break;
     }
 
     return points;
-}
-
-void alloc_qtable(int row, int col, int num_actions){
-    double ** qtable = calloc((row * col) * sizeof(double*));
-    for (int i=1; i < (row * col); ++i){
-        qtable[i] = calloc( num_actions * sizeof(double*));
-    }
 }
 
 //FInd the best action for a given state
@@ -64,22 +80,12 @@ int Max_action(int row, int col){
     return temp_action;
 }
 
-//determine the qvalue for a (s,a)
+//determine the qvalue for a (state,action)
 double qfunction(int row, int col, int action){
-    // Exploration factor
-    double epsilon = 0.1 ;
-    double gamma = 0.9 ;
     double qvalue;
     int state = rows * row + col;
 
-    qtable [state , action] = qtable [state,action] + 
-
-    for (int i=1; i < (row * col); ++i){
-        for (int j=1; j < number_actions; ++j){
-            int new_state;
-            qtable[i][j] = qtable[i][j] + epsilon * (reward(i) + gamma *Max_action(row,col) - qtable[i][j]);
-        }
-    }
+    qvalue = qtable [state,action] + epsilon * (reward(row,col) + gamma *Max_action(row,col) - qtable [state , action]); 
 
     return qvalue;
 }
@@ -90,8 +96,29 @@ int qlearning(int row, int col){
 }
 
 int main(){
-   maze_make("maze.txt");
-   init_visited();
+    double **qtable = calloc(rows * cols,sizeof(double*));
+    for (int i=1; i < (rows * cols); ++i){
+        qtable[i] = calloc(number_actions, sizeof(double*));
+    }
+
+    maze_reset();
+    maze_make("maze.txt");
+    init_visited();
+    alloc_qtable();
+    int visited[rows][cols];
+
+
+
+    int rowCounter = 0; 
+    while(maze[start_row][start_col] != 'g'){
+        //indicate the current location
+        maze[state_row][state_col] = 'x';
+
+        switch(maze[state_row][state_col]){
+            
+        }
+    }
+
 
    printf("%d, %d \n", rows, cols);
    printf("number of actions :  %d \n", number_actions); 
